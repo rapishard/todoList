02e21ff8c5a020e5542008ae1e9702faa7d3a711
rapishard <alessandrorapisarda1981@gmail.com>
2022-04-22 09:23:45 +0200

    font update2

--- src/App.tsx
+++ src/App.tsx
@@ -62,7 +62,7 @@ const App: React.FC = () => {
   return (
     <DragDropContext onDragEnd={onDragEnd}>
       <div className="App">
-        <span className="heading">Taskify</span>
+        <span className="heading">Todolist</span>
         <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
         <TodoList
           todos={todos}
